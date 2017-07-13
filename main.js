235cdee8fc78d400419b36678281a1f3c0c10b76
Your name <Your email>
2017-07-12 22:54:14 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,38 @@
+console.log(customers);
+
+for (var i = 0; i < customers.results.length; i++) {
+  // console.log(customers);
+  console.log(customers.results[i].name.first);
+}
+
+// create variables
+
+
+// create divs, imgs, and ps.
+
+
+// create the div
+// give the div a class=one
+// append it to the body
+// create the h1
+
+// append it to the div
+//create another div (a lot of them)
+//give it a class=two
+
+//create a third set of div's
+// append them to div class two
+//
+//create several img and p
+// create p  class="name" 
+//append them to the third set of divs
+//
+//populate the img and p
+//
+//
+//
+//
+//
+//
+//
+//
