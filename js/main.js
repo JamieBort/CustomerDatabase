8379e4c48a0e93c1a9f4ac20f8c9335fef42b5f1
Jamie Bort <jamiebort@gmail.com>
2018-02-14 15:04:28 -0500

    First draft.

--- js/main.js
+++ js/main.js
@@ -1,43 +1,34 @@
+console.log(customers.info);
 console.log(customers.info.seed);
+console.log(customers.info.results);
+console.log(customers.info.page);
+console.log(customers.info.version);
 
-let wholePage = document.querySelector("body");
-  let employeeProfile = document.createElement("div");
+console.log(customers.results);
+console.log(customers.results[0]);
+console.log(customers.results[0].gender);
+console.log(customers.results[0].name);
+console.log(customers.results[0].name.title);
+console.log(customers.results[0].name.first);
+console.log(customers.results[0].name.last);
+console.log(customers.results[0].location);
+console.log(customers.results[0].phone);
+console.log(customers.results[0].id.value);
 
-  let employeeDirectory =
-    `<img src="${customers.results[0].picture.large}">
-    <h3>${customers.results[0].name.first} ${customers.results[0].name.last}</h3>
 
-    <ul>
-      <li>${customers.results[0].email}</li>
-      <li>${customers.results[0].location.street}</li>
-      <li>${customers.results[0].location.city}</li>
-      <li>${customers.results[0].location.state}</li>
-      <li>${customers.results[0].location.postcode}</li>
-      <li>${customers.results[0].phone}</li>
-      <li>${customers.results[0].id.value}</li>
-    </ul>`;
+// console.log(customers.results[0].picture.thumbnail);
 
-    employeeProfile.innerHTML = employeeDirectory;
-    wholePage.appendChild(employeeProfile);
+let body = document.querySelector("body");
+let employeeProfile = document.createElement("div");
+let employeeInfo = `<img src="${customers.results[0].picture.large}">
+<h3>${customers.results[0].name.first} ${customers.results[0].name.last}</h3>
+<ul>${customers.results[0].email}</ul>
+<ul>${customers.results[0].location.street}</ul>
+<ul>${customers.results[0].location.city}</ul>
+<ul>${customers.results[0].location.state}</ul>
+<ul>${customers.results[0].location.postcode}</ul>
+<ul>${customers.results[0].phone}</ul>
+<ul>${customers.results[0].id.value}</ul>`
 
-// ===================
-
-    let wholePage1 = document.querySelector("body");
-      let employeeProfile1 = document.createElement("div");
-
-      let employeeDirectory1 =
-        `<img src="${customers.results[1].picture.large}">
-        <h3>${customers.results[1].name.first} ${customers.results[1].name.last}</h3>
-
-        <ul>
-          <li>${customers.results[1].email}</li>
-          <li>${customers.results[1].location.street}</li>
-          <li>${customers.results[1].location.city}</li>
-          <li>${customers.results[1].location.state}</li>
-          <li>${customers.results[1].location.postcode}</li>
-          <li>${customers.results[1].phone}</li>
-          <li>${customers.results[1].id.value}</li>
-        </ul>`;
-
-        employeeProfile1.innerHTML = employeeDirectory1;
-        wholePage1.appendChild(employeeProfile1);
+employeeProfile.innerHTML = employeeInfo;
+body.appendChild(employeeProfile);
