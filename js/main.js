3a496a25cce64c195f0f697b18637ed380dfa003
Jamie Bort <jamiebort@gmail.com>
2018-02-14 15:08:40 -0500

    Second draft.

--- js/main.js
+++ js/main.js
@@ -16,7 +16,6 @@ console.log(customers.results[0].phone);
 console.log(customers.results[0].id.value);
 
 
-// console.log(customers.results[0].picture.thumbnail);
 
 let body = document.querySelector("body");
 let employeeProfile = document.createElement("div");
@@ -32,3 +31,20 @@ let employeeInfo = `<img src="${customers.results[0].picture.large}">
 
 employeeProfile.innerHTML = employeeInfo;
 body.appendChild(employeeProfile);
+
+// =================================
+
+let body2 = document.querySelector("body");
+let employeeProfile2 = document.createElement("div");
+let employeeInfo2 = `<img src="${customers.results[1].picture.large}">
+<h3>${customers.results[1].name.first} ${customers.results[1].name.last}</h3>
+<ul>${customers.results[1].email}</ul>
+<ul>${customers.results[1].location.street}</ul>
+<ul>${customers.results[1].location.city}</ul>
+<ul>${customers.results[1].location.state}</ul>
+<ul>${customers.results[1].location.postcode}</ul>
+<ul>${customers.results[1].phone}</ul>
+<ul>${customers.results[1].id.value}</ul>`
+
+employeeProfile2.innerHTML = employeeInfo2;
+body2.appendChild(employeeProfile2);
